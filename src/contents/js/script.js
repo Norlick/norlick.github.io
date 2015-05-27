d890cb2140a9a1c66f32c17726ad615ba3ec5b91
Nathan Orlick <norlick@gmail.com>
2015-05-27 09:38:02 +0930

    More smithing

--- src/contents/js/script.js
+++ src/contents/js/script.js
@@ -1,3 +1,20 @@
+// If we're browsing in portrait, flag the stylesheet as such
+function layoutSwitch() {
+	if ( innerHeight > innerWidth ) {
+		document.querySelector('body').id = 'Portrait';
+	} else {
+		document.querySelector('body').id = '';
+	}
+}
+
+window.addEventListener( 'resize', function() {
+	requestAnimationFrame( layoutSwitch );
+} );
+
+requestAnimationFrame( layoutSwitch );
+
+
+// ONLOAD Stuff
 onload = function() {
 	console.log('BOO!');
 };
